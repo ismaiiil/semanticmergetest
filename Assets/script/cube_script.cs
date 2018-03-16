16a31fd27d06313722281a791e7bd16d5bca508e
mael <ismaiiil@live.com>
2018-03-16 21:40:20 +0400

    ss

--- Assets/script/cube_script.cs
+++ Assets/script/cube_script.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cube_script : MonoBehaviour {
+    public GameObject cubiii1;
+    public GameObject cubiii2;
+    public GameObject cubiii3;
+    public GameObject cubiii4;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
